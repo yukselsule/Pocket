cf64446a542c3aeb9dd23cf42d7a5a643784df50
yukselsule <sulecantekin@gmail.com>
2024-09-02 17:23:04 +0300

    style: 💄 main element style

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -3,15 +3,15 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 function AppLayout() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main className="container mx-auto my-0 w-3/4 pt-12">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default AppLayout;

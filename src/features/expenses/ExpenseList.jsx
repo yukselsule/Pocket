b38794a7a7a596e975da07a38ed3bd28a6837d12
yukselsule <sulecantekin@gmail.com>
2024-09-02 17:23:39 +0300

    style: 💄 list elements styled

--- src/features/expenses/ExpenseList.jsx
+++ src/features/expenses/ExpenseList.jsx
@@ -3,21 +3,25 @@ import { useSelector } from "react-redux";
 import { selectExpenses } from "./ExpenseSlice";
 
 function ExpenseList() {
-  const expenses = useSelector(selectExpenses);
+    const expenses = useSelector(selectExpenses);
 
-  return (
-    <ul>
-      {expenses
-        .slice()
-        .reverse()
-        .map((expense, index) => (
-          <li key={index}>
-            {expense.expenseType} - {expense.description} :{" "}
-            <span>{expense.amount}</span>
-          </li>
-        ))}
-    </ul>
-  );
+    return (
+        <ul className="mt-4 overflow-hidden rounded-md border border-red-500 bg-stone-100">
+            {expenses.length > 0 &&
+                expenses
+                    .slice()
+                    .reverse()
+                    .map((expense, index) => (
+                        <li
+                            key={index}
+                            className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} px-3 py-1`}
+                        >
+                            {expense.expenseType} - {expense.description} :{" "}
+                            <span>{expense.amount}</span>
+                        </li>
+                    ))}
+        </ul>
+    );
 }
 
 export default ExpenseList;
--- src/features/income/IncomeList.jsx
+++ src/features/income/IncomeList.jsx
@@ -3,20 +3,24 @@ import { useSelector } from "react-redux";
 import { selectIncomes } from "./IncomeSlice";
 
 function IncomeList() {
-  const incomes = useSelector(selectIncomes);
-
-  return (
-    <ul>
-      {incomes
-        .slice()
-        .reverse()
-        .map((income, index) => (
-          <li key={index}>
-            {income.description} : <span>{income.amount}</span>
-          </li>
-        ))}
-    </ul>
-  );
+    const incomes = useSelector(selectIncomes);
+   
+    return (
+        <ul className="mt-4 overflow-hidden rounded-md border border-red-500 bg-stone-100">
+            {incomes.length > 0 &&
+                incomes
+                    ?.slice()
+                    .reverse()
+                    .map((income, index) => (
+                        <li
+                            key={index}
+                            className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} px-3 py-1`}
+                        >
+                            {income.description} : <span>{income.amount}</span>
+                        </li>
+                    ))}
+        </ul>
+    );
 }
 
 export default IncomeList;

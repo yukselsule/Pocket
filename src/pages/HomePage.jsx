efec4459c5865159bab07afdb276d2c40b03ab47
yukselsule <sulecantekin@gmail.com>
2024-09-02 17:22:44 +0300

    style: 💄 h2 elements added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,23 @@
-import Balance from "../features/balance/Balance";
 import ExpenseForm from "../features/expenses/ExpenseForm";
 import ExpenseList from "../features/expenses/ExpenseList";
 import IncomeForm from "../features/income/IncomeForm";
 import IncomeList from "../features/income/IncomeList";
 
 function HomePage() {
-  return (
-    <div>
-      <Balance />
-      <div>
-        <IncomeForm />
-        <IncomeList />
-      </div>
-      <div>
-        <ExpenseForm />
-        <ExpenseList />
-      </div>
-    </div>
-  );
+    return (
+        <div className="md:flex md:justify-between">
+            <div className="rounded-md bg-sky-100 px-12 py-6">
+                <h2 className="font-bold uppercase text-red-950">Incomes</h2>
+                <IncomeForm />
+                <IncomeList />
+            </div>
+            <div className="rounded-md bg-sky-100 px-12 py-6">
+                <h2 className="font-bold uppercase text-red-950">Expenses</h2>
+                <ExpenseForm />
+                <ExpenseList />
+            </div>
+        </div>
+    );
 }
 
 export default HomePage;

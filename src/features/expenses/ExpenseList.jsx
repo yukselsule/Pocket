f08aeb1b58904a7f4e88a1c508205b17fff3b963
yukselsule <sulecantekin@gmail.com>
2024-09-06 12:35:00 +0300

    style: 💄 List styles added

--- src/features/expenses/ExpenseList.jsx
+++ src/features/expenses/ExpenseList.jsx
@@ -6,21 +6,27 @@ function ExpenseList() {
     const expenses = useSelector(selectExpenses);
 
     return (
-        <ul className="mt-4 overflow-hidden rounded-md border border-red-500 bg-stone-100">
-            {expenses.length > 0 &&
-                expenses
-                    .slice()
-                    .reverse()
-                    .map((expense, index) => (
-                        <li
-                            key={index}
-                            className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} px-3 py-1`}
-                        >
-                            {expense.expenseType} - {expense.description} :{" "}
-                            <span>{expense.amount}</span>
-                        </li>
-                    ))}
-        </ul>
+        <div className="max-w-68 mt-6 self-center overflow-hidden rounded-sm border border-rose-300 bg-stone-100 p-0 md:max-w-96">
+            <ul className="thin-scrollbar max-h-60 max-w-80 overflow-y-scroll scroll-smooth md:max-w-full">
+                {expenses.length > 0 &&
+                    expenses
+                        .slice()
+                        .reverse()
+                        .map((expense, index) => (
+                            <li
+                                key={index}
+                                className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} flex justify-between gap-3 px-3 py-1`}
+                            >
+                                <span className="text-wrap">
+                                    {expense.expenseType} -{" "}
+                                    {expense.description} :{" "}
+                                </span>
+
+                                <span>{expense.amount}</span>
+                            </li>
+                        ))}
+            </ul>
+        </div>
     );
 }
 
--- src/features/income/IncomeList.jsx
+++ src/features/income/IncomeList.jsx
@@ -4,22 +4,25 @@ import { selectIncomes } from "./IncomeSlice";
 
 function IncomeList() {
     const incomes = useSelector(selectIncomes);
-   
+
     return (
-        <ul className="mt-4 overflow-hidden rounded-md border border-red-500 bg-stone-100">
-            {incomes.length > 0 &&
-                incomes
-                    ?.slice()
-                    .reverse()
-                    .map((income, index) => (
-                        <li
-                            key={index}
-                            className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} px-3 py-1`}
-                        >
-                            {income.description} : <span>{income.amount}</span>
-                        </li>
-                    ))}
-        </ul>
+        <div className="max-w-68 mt-6 self-center overflow-hidden rounded-sm border border-sky-300 bg-stone-100 p-0 md:max-w-96">
+            <ul className="thin-scrollbar max-h-60 max-w-80 overflow-y-scroll scroll-smooth">
+                {incomes.length > 0 &&
+                    incomes
+                        ?.slice()
+                        .reverse()
+                        .map((income, index) => (
+                            <li
+                                key={index}
+                                className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} px-3 py-1`}
+                            >
+                                {income.description} :{" "}
+                                <span>{income.amount}</span>
+                            </li>
+                        ))}
+            </ul>
+        </div>
     );
 }
 

25943a089cdf4f2b51630695e5e620f4cee7eb2b
yukselsule <sulecantekin@gmail.com>
2024-09-09 10:23:47 +0300

    style: 💄 Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,31 @@
-import logo from "../assets/images/pocket-logo.png";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { selectBalance } from "../features/balance/BalanceSlice";
 import Balance from "../features/balance/Balance";
 
+import logo from "../assets/images/pocket-logo.png";
+
 function Header() {
-  return (
-    <header>
-      <img src={logo} alt="pocket-logo" />
-      <Balance />
-    </header>
-  );
+    const balance = useSelector(selectBalance);
+
+    return (
+        <header className="flex items-center justify-between bg-stone-900 px-5 py-2 text-stone-100 sm:px-10 sm:py-3">
+            <Link to="/">
+                <img className="w-14" src={logo} alt="pocket-logo" />
+            </Link>
+
+            {balance ? (
+                <div className="flex items-center justify-between gap-2">
+                    <Balance />
+                    <span>/</span>
+                    <Link to="/summary">See Summary</Link>
+                </div>
+            ) : (
+                <p>Keep track of your wallet</p>
+            )}
+        </header>
+    );
 }
 
 export default Header;

1d46b18f5491acb8bc009b3794efea1c303c88b8
yukselsule <sulecantekin@gmail.com>
2024-09-09 10:23:13 +0300

    style: 💄 minor fixes

--- src/features/balance/Balance.jsx
+++ src/features/balance/Balance.jsx
@@ -3,13 +3,13 @@ import { useSelector } from "react-redux";
 import { selectBalance } from "./BalanceSlice";
 
 function Balance() {
-  const balance = useSelector(selectBalance);
+    const balance = useSelector(selectBalance);
 
-  return (
-    <div>
-      Balance: <span>{balance}</span>
-    </div>
-  );
+    return (
+        <div>
+            Balance: <span className="text-lg text-sky-200">{balance}</span>
+        </div>
+    );
 }
 
 export default Balance;

3306f820bbd08464c2354b069f413840c284864c
yukselsule <sulecantekin@gmail.com>
2024-09-13 15:29:57 +0300

    feat: 🎸 Footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 function Footer() {
-  return <footer>Footer</footer>;
+    return (
+        <footer className="bg-stone-900 px-4 py-2 text-center text-stone-100">
+            <p>Keep track of your wallet!</p>
+        </footer>
+    );
 }
 
 export default Footer;

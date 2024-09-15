b3c1f86a9d4e06e2277177cefd41c8b7ccb0e240
yukselsule <sulecantekin@gmail.com>
2024-09-15 12:19:15 +0300

    style: 💄 padding added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import IncomeList from "../features/income/IncomeList";
 function HomePage() {
     return (
         <div className="flex flex-col gap-8 px-4 py-4 lg:flex-row lg:justify-between">
-            <div className="flex max-w-lg flex-col justify-between rounded-md bg-sky-100 px-4 py-2 sm:min-w-96 sm:px-12 sm:py-6">
+            <div className="flex max-w-lg flex-col justify-between rounded-md bg-sky-100 px-6 py-3 sm:min-w-96 sm:px-12 sm:py-6">
                 <div>
                     <h2 className="pb-4 font-bold uppercase text-sky-950">
                         Incomes
@@ -15,7 +15,7 @@ function HomePage() {
                 </div>
                 <IncomeList />
             </div>
-            <div className="flex max-w-lg flex-col justify-between rounded-md bg-rose-100 px-4 py-2 sm:min-w-96 sm:px-12 sm:py-6">
+            <div className="flex max-w-lg flex-col justify-between rounded-md bg-rose-100 px-6 py-3 sm:min-w-96 sm:px-12 sm:py-6">
                 <div>
                     <h2 className="pb-4 font-bold uppercase text-rose-950">
                         Expenses

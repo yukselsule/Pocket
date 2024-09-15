23a5de8b28cbc77cc2a0998ae40f91b5d5e70219
yukselsule <sulecantekin@gmail.com>
2024-09-15 12:17:34 +0300

    feat: 🎸 conditional rendering

--- src/features/expenses/ExpenseList.jsx
+++ src/features/expenses/ExpenseList.jsx
@@ -5,29 +5,30 @@ import { selectExpenses } from "./ExpenseSlice";
 function ExpenseList() {
     const expenses = useSelector(selectExpenses);
 
-    return (
-        <div className="max-w-68 mt-6 w-full self-center overflow-hidden rounded-sm border border-rose-300 bg-stone-100 p-0 md:max-w-96">
-            <ul className="thin-scrollbar max-h-60 overflow-y-scroll scroll-smooth">
-                {expenses.length > 0 &&
-                    expenses
-                        .slice()
-                        .reverse()
-                        .map((expense, index) => (
-                            <li
-                                key={index}
-                                className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} flex justify-between gap-3 px-3 py-1`}
-                            >
-                                <span className="text-wrap">
-                                    {expense.expenseType} -{" "}
-                                    {expense.description} :{" "}
-                                </span>
+    if (expenses.length > 0)
+        return (
+            <div className="max-w-68 mt-6 w-full self-center overflow-hidden rounded-sm border border-rose-300 bg-stone-100 md:max-w-96">
+                <ul className="thin-scrollbar max-h-60 overflow-y-scroll scroll-smooth">
+                    {expenses.length > 0 &&
+                        expenses
+                            .slice()
+                            .reverse()
+                            .map((expense, index) => (
+                                <li
+                                    key={index}
+                                    className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} flex justify-between gap-3 px-3 py-1`}
+                                >
+                                    <span className="text-wrap">
+                                        {expense.expenseType} -{" "}
+                                        {expense.description} :{" "}
+                                    </span>
 
-                                <span>{expense.amount}</span>
-                            </li>
-                        ))}
-            </ul>
-        </div>
-    );
+                                    <span>{expense.amount}</span>
+                                </li>
+                            ))}
+                </ul>
+            </div>
+        );
 }
 
 export default ExpenseList;
--- src/features/income/IncomeList.jsx
+++ src/features/income/IncomeList.jsx
@@ -5,25 +5,26 @@ import { selectIncomes } from "./IncomeSlice";
 function IncomeList() {
     const incomes = useSelector(selectIncomes);
 
-    return (
-        <div className="max-w-68 mt-6 w-full self-center overflow-hidden rounded-sm border border-sky-300 bg-stone-100 p-0 md:max-w-96">
-            <ul className="thin-scrollbar max-h-60 overflow-y-scroll scroll-smooth">
-                {incomes.length > 0 &&
-                    incomes
-                        ?.slice()
-                        .reverse()
-                        .map((income, index) => (
-                            <li
-                                key={index}
-                                className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} flex justify-between gap-3 px-3 py-1`}
-                            >
-                                {income.description} :{" "}
-                                <span>{income.amount}</span>
-                            </li>
-                        ))}
-            </ul>
-        </div>
-    );
+    if (incomes.length > 0)
+        return (
+            <div className="max-w-68 mt-6 w-full self-center overflow-hidden rounded-sm border border-sky-300 bg-stone-100 md:max-w-96">
+                <ul className="thin-scrollbar max-h-60 overflow-y-scroll scroll-smooth">
+                    {incomes.length > 0 &&
+                        incomes
+                            ?.slice()
+                            .reverse()
+                            .map((income, index) => (
+                                <li
+                                    key={index}
+                                    className={`${index % 2 === 0 ? "bg-stone-100" : "bg-stone-300"} flex justify-between gap-3 px-3 py-1`}
+                                >
+                                    {income.description} :{" "}
+                                    <span>{income.amount}</span>
+                                </li>
+                            ))}
+                </ul>
+            </div>
+        );
 }
 
 export default IncomeList;
